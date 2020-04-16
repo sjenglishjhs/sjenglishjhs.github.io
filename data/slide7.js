152ad35cd6f24cf2dbb1106f31cf0a8488a0c140
sjenglishjhs <63623525+sjenglishjhs@users.noreply.github.com>
2020-04-16 18:42:34 +0800

    del
    dd

--- data/slide7.js
+++
@@ -1,5 +0,0 @@
-(function(){var loadHandler=window['sl_{E4542051-B313-4EBA-8F33-223724560169}'];loadHandler&&loadHandler(6, '<div id="spr0_7d406bf1"><div id="spr1_7d406bf1" class="kern slide"><img id="img2_7d406bf1" src="data/img1.png" width="960px" height="540px" alt="" style="left:0px;top:0px;"/><div id="svg0_7d406bf1" style="left:18.2px;top:19.2px;"><svg width="924" height="503" viewBox="0 0 924 503"><path fill="#ffffff" d="M0,0 h923.2 v502.2 h-923.2 Z"/></svg></div></div><div id="spr2_7d406bf1" class="kern slide"><div id="spr3_7d406bf1" style="left:86.851px;top:22.396px;"><img id="img0_7d406bf1" src="data/img16.png" width="341" height="495" alt="A screenshot of a cell phone\
-\
-Description automatically generated" style="left:0.075px;top:0.104px;"/></div><div id="spr4_7d406bf1" style="left:498.4px;top:22.396px;"><img id="img1_7d406bf1" src="data/img17.png" width="343" height="495" alt="A screenshot of a cell phone\
-\
-Description automatically generated" style="left:0.076px;top:0.104px;"/></div></div></div>', '{"s":[]}');})();
